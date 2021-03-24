9d013f42c1b5c57d114b6d99ef6ba93857b90910
Artur <green-shrek@inbox.ru>
2021-03-24 20:08:49 +0300

    Добавленее страница редактирования поста

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 from django.shortcuts import get_object_or_404
 from django.core.paginator import Paginator
+from django.urls import reverse
 
 from .models import Post, Group, User
 from .forms import PostForm
@@ -58,18 +59,23 @@ def post_view(request, username, post_id):
     post = get_object_or_404(Post,id = post_id)
     post_list = Post.objects.filter(author = user)
     post_count = post_list.count()
-    
+    user_now = request.user
     return render(
         request,
         'post.html',
         {'post': post,
         'user': user,
-        'post_count': post_count})
+        'post_count': post_count,
+        'user_now': user_now,})
 
-
+@login_required
 def post_edit(request, username, post_id):
-    # тут тело функции. Не забудьте проверить, 
-    # что текущий пользователь — это автор записи.
-    # В качестве шаблона страницы редактирования укажите шаблон создания новой записи
-    # который вы создали раньше (вы могли назвать шаблон иначе)
-    return render(request, 'new.html', {}) 
+    post = get_object_or_404(Post, author__username=username, id=post_id)
+    if post.author.username != request.user.username:
+        return redirect(reverse('profile', args=[post.author.username]))
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        post.save()
+        return redirect(reverse('profile', args=[post.author.username]))
+    #form = PostForm(instance=post)
+    return render(request, 'new.html', {'form' : form, 'post': post,'edit':True}) 
